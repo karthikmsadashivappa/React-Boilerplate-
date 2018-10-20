1eb340b26151934b7c258ade0908e7d29203b0ca
karthik <ms18karthik@gmail.com>
2018-10-20 21:37:50 +0530

    Adding React and React Dom to the project👏

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+render(<App />, document.querySelector("#App"));
